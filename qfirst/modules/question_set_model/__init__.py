9a91f0a8c89ac68bcbabd52208637342f72a48f1
julianmichael <julianjohnmichael@gmail.com>
2018-08-20 17:20:08 -0700

    Remove question set models

--- qfirst/modules/question_set_model/__init__.py
+++
@@ -1,2 +0,0 @@
-from qfirst.modules.question_set_model.question_set_model import QuestionSetModel
-from qfirst.modules.question_set_model.sequential_question_set_model import SequentialQuestionSetModel
