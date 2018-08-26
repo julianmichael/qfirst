8e4e440b26d5942a09318ab955c7fa8bbb7f9743
Julian Michael <julianjohnmichael@gmail.com>
2018-08-26 02:58:16 +0000

    Fix up details of model assembly and eval, plus run some evals

--- qfirst/metrics/qfirst_beam_metric/beam_filtering_metric.py
+++ qfirst/metrics/qfirst_beam_metric/beam_filtering_metric.py
@@ -22,19 +22,19 @@ class BeamFilteringMetric(QfirstBeamMetric):
                  metadata):
         filtered_beam = self.beam_filter(full_beam)
 
-        print("\n" + " ".join(metadata["sentence_tokens"]))
-        print("\nGOLD:")
-        for gold_entry in gold_qa_pairs:
-            print("\n" + " ".join(gold_entry["question"]))
-            span_strings = [" ".join(metadata["sentence_tokens"][span.start() : (span.end() + 1)]) for span in gold_entry["answer_spans"]]
-            print("   " + "\n   ".join(span_strings))
-        print("\nPREDICTED:")
-        for entry in full_beam:
-            if entry["question_prob"] > 0.02:
-                print("\n" + ("%4.2f (%4.2f) " % (entry["question_prob"], entry["invalidity_prob"])) + " ".join(entry["question"]))
-                sorted_spans = sorted(entry["spans"], key = lambda t: t[1], reverse = True)
-                span_strings = [" ".join(metadata["sentence_tokens"][span.start() : (span.end() + 1)]) + (" (%.2f)" % prob) for span, prob in sorted_spans if prob > 0.01]
-                print("   " + ("\n   ").join(span_strings))
+        # print("\n" + " ".join(metadata["sentence_tokens"]))
+        # print("\nGOLD:")
+        # for gold_entry in gold_qa_pairs:
+        #     print("\n" + " ".join(gold_entry["question"]))
+        #     span_strings = [" ".join(metadata["sentence_tokens"][span.start() : (span.end() + 1)]) for span in gold_entry["answer_spans"]]
+        #     print("   " + "\n   ".join(span_strings))
+        # print("\nPREDICTED:")
+        # for entry in full_beam:
+        #     if entry["question_prob"] > 0.02:
+        #         print("\n" + ("%4.2f (%4.2f) " % (entry["question_prob"], entry["invalidity_prob"])) + " ".join(entry["question"]))
+        #         sorted_spans = sorted(entry["spans"], key = lambda t: t[1], reverse = True)
+        #         span_strings = [" ".join(metadata["sentence_tokens"][span.start() : (span.end() + 1)]) + (" (%.2f)" % prob) for span, prob in sorted_spans if prob > 0.01]
+        #         print("   " + ("\n   ").join(span_strings))
 
         self.downstream_metric(gold_qa_pairs, filtered_beam)
 
--- scripts/evaluate_all_models.sh
+++ scripts/evaluate_all_models.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # qfirst model, tuning (dense)
-python -m allennlp.run evaluate models/qfirst-model-2.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"question_source": "turk", "min_answers": 6, "min_valid_answers": 5}}'
-# qfirst model, eval (dense). TODO fill in with tuned thresholds
-python -m allennlp.run evaluate models/qfirst-model-2.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"question_source": "turk", "min_answers": 6, "min_valid_answers": 5}, "metric": {"type": "beam_filtering", "filter": {"question_threshold": TODO, "span_threshold": TODO, "invalid_threshold": TODO}}'
+python -m allennlp.run evaluate models/qfirst-model-1.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"question_source": "turk", "min_answers": 6, "min_valid_answers": 5}}'
+# qfirst model, eval (dense). TODO replace with final tuned thresholds
+python -m allennlp.run evaluate models/qfirst-model-1.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"question_source": "turk", "min_answers": 6, "min_valid_answers": 5}, "model": {"metric": {"type": "beam_filtering", "filter": {"question_threshold": 0.1, "span_threshold": 0.5, "invalid_threshold": 0.25}}}}'
 # qfirst model, tuning (non-dense)
-python -m allennlp.run evaluate models/qfirst-model-2.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"target": "e2e-f1"} }'
+python -m allennlp.run evaluate models/qfirst-model-1.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"target": "e2e-f1"} }'
 # qfirst model, eval (non-dense). TODO fill in with tuned thresholds
-python -m allennlp.run evaluate models/qfirst-model-2.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"min_answers": 6, "min_valid_answers": 5}, "metric": {"type": "beam_filtering", "filter": {"question_threshold": TODO, "span_threshold": TODO, "invalid_threshold": TODO}}'
+python -m allennlp.run evaluate models/qfirst-model-1.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{"dataset_reader": {"min_answers": 6, "min_valid_answers": 5}, "metric": {"type": "beam_filtering", "filter": {"question_threshold": TODO, "span_threshold": TODO, "invalid_threshold": TODO}}'
 
 # afirst model, tuning on turk-question eval (non-dense)
-python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{ "dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"target": "e2e-f1"} }'
+python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device 0 --include-package qfirst --overrides '{ "dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"target": "e2e-f1"} }'
 # afirst model, running on turk-question eval (non-dense)
-python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{ "dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"type": "threshold", "span_threshold": 0.5} }'
+python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/orig/dev.jsonl.gz --cuda-device 0 --include-package qfirst --overrides '{ "dataset_reader": {"min_answers": 3, "min_valid_answers": 3}, "metric": {"type": "threshold", "span_threshold": 0.5} }'
 
 # afirst model, tuning on turk-question eval (dense)
-python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{ "dataset_reader": {"question_sources": "turk", "min_answers": 6, "min_valid_answers": 5} }'
+python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device 0 --include-package qfirst --overrides '{ "dataset_reader": {"question_sources": "turk", "min_answers": 6, "min_valid_answers": 5} }'
 
-# afirst model, running eval with tuned threshold (dense). # TODO replace with final tuned threshold
-python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device -1 --include-package qfirst --overrides '{ "dataset_reader": {"question_sources": "turk", "min_answers": 6, "min_valid_answers": 5}, "metric": {"type": "threshold", "span_threshold": 0.5} }'
+# afirst model, running eval with tuned threshold (dense).
+python -m allennlp.run evaluate models/afirst-model.tar.gz --evaluation-data-file http://qasrl.org/data/qasrl-v2/dense/dev.jsonl.gz --cuda-device 0 --include-package qfirst --overrides '{ "dataset_reader": {"question_sources": "turk", "min_answers": 6, "min_valid_answers": 5}, "metric": {"type": "threshold", "span_threshold": 0.7} }'
