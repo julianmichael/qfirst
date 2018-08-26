f9edd911cb71bb1ed18d2421f0775c7048ae1f00
julianmichael <julianjohnmichael@gmail.com>
2018-08-25 18:36:32 -0700

    Update nrl dependency
    Also change model directory structure a tiny bit

--- scripts/prepare_all_models.sh
+++ scripts/prepare_all_models.sh
@@ -4,17 +4,17 @@
 #        --qg models/qg/cond-lm-1-passthrough \
 #        --qa models/qa/simple-1-weighted \
 #        --config_base models/qfirst-parser-base.json \
-#        --out models/full/qfirst-model-1.tar.gz \
+#        --out models/qfirst-model-1.tar.gz \
 #        --target_device cpu
 
 # python scripts/assemble_qfirst_model.py \
 #        --qg models/qg/cond-lm-2-bilstm \
 #        --qa models/qa/simple-1-union \
 #        --config_base models/qfirst-parser-base.json \
-#        --out models/full/qfirst-model-2.tar.gz \
+#        --out models/qfirst-model-2.tar.gz \
 #        --target_device cpu
 
 # python scripts/prepare_afirst_model.py \
 #        --model_dir data/qasrl_parser_elmo \
-#        --out models/full/afirst-model.tar.gz \
+#        --out models/afirst-model.tar.gz \
 #        --target_device cpu
