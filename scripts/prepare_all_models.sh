f47aa54819b670d3ebffd591c0b528dde3e43296
julianmichael <julianjohnmichael@gmail.com>
2018-08-25 17:24:31 -0700

    Prepare to run validation tuning on gpu

--- scripts/prepare_all_models.sh
+++ scripts/prepare_all_models.sh
@@ -7,12 +7,12 @@
 #        --out models/full/qfirst-model-1.tar.gz \
 #        --target_device cpu
 
-python scripts/assemble_qfirst_model.py \
-       --qg models/qg/cond-lm-2-bilstm \
-       --qa models/qa/simple-1-union \
-       --config_base models/qfirst-parser-base.json \
-       --out models/full/qfirst-model-2.tar.gz \
-       --target_device cpu
+# python scripts/assemble_qfirst_model.py \
+#        --qg models/qg/cond-lm-2-bilstm \
+#        --qa models/qa/simple-1-union \
+#        --config_base models/qfirst-parser-base.json \
+#        --out models/full/qfirst-model-2.tar.gz \
+#        --target_device cpu
 
 # python scripts/prepare_afirst_model.py \
 #        --model_dir data/qasrl_parser_elmo \
