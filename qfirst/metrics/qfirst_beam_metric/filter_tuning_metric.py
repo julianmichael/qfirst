5d1341294c068ebddb023d09f5244e2a1fd5c128
julianmichael <julianjohnmichael@gmail.com>
2018-08-31 02:47:43 -0700

    Add single_answer_only and invalid_as_threshold beam filter params

--- qfirst/metrics/qfirst_beam_metric/filter_tuning_metric.py
+++ qfirst/metrics/qfirst_beam_metric/filter_tuning_metric.py
@@ -15,6 +15,8 @@ class FilterTuningMetric(QfirstBeamMetric):
                  question_thresholds: List[float],
                  span_thresholds: List[float],
                  invalid_thresholds: List[float],
+                 try_first_answer_only: bool = False,
+                 try_invalid_as_threshold: bool = False,
                  use_dense_metric: bool = False,
                  recall_constraint: float = 2.0):
         self.target = target
@@ -24,11 +26,15 @@ class FilterTuningMetric(QfirstBeamMetric):
             else:
                 return EndToEndMetric()
 
+        first_answer_bools = [True, False] if try_first_answer_only else [False]
+        invalid_as_threshold_bools = [True, False] if try_invalid_as_threshold else [False]
         self.filter_metrics = [
-            (BeamFilter(q_thresh, s_thresh, i_thresh), make_metric())
+            (BeamFilter(q_thresh, s_thresh, i_thresh, invalid_as_threshold, first_answer_only), make_metric())
             for q_thresh in question_thresholds
             for s_thresh in span_thresholds
             for i_thresh in invalid_thresholds
+            for first_answer_only in first_answer_bools
+            for invalid_as_threshold in invalid_as_threshold_bools
         ]
         for _, m in self.filter_metrics:
             m.reset()
@@ -59,7 +65,9 @@ class FilterTuningMetric(QfirstBeamMetric):
         filter_params = {
             "question-threshold": best_filter.question_threshold,
             "span-threshold": best_filter.span_threshold,
-            "invalid-threshold": best_filter.invalid_threshold
+            "invalid-threshold": best_filter.invalid_threshold,
+            "first_answer_only": float(best_filter.first_answer_only),
+            "invalid_as_threshold": float(best_filter.invalid_as_threshold)
         }
         return {**filter_params, **best_metrics}
 
@@ -69,9 +77,13 @@ class FilterTuningMetric(QfirstBeamMetric):
         question_thresholds = params.pop("question_thresholds", [0.01, 0.02, 0.05, 0.1, 0.15])
         span_thresholds = params.pop("span_thresholds", [0.25, 0.30, 0.35, 0.40, 0.45])
         invalid_thresholds = params.pop("invalid_thresholds", [0.02, 0.05, 0.10, 0.20, 0.30])
+        try_first_answer_only = params.pop("try_first_answer_only", False)
+        try_invalid_as_threshold = params.pop("try_invalid_as_threshold", False)
         use_dense_metric = params.pop("use_dense_metric", False)
         return FilterTuningMetric(target = target,
                                   question_thresholds = question_thresholds,
                                   span_thresholds = span_thresholds,
                                   invalid_thresholds = invalid_thresholds,
+                                  try_first_answer_only = try_first_answer_only,
+                                  try_invalid_as_threshold = try_invalid_as_threshold,
                                   use_dense_metric = use_dense_metric)
--- qfirst/util/beam_filter.py
+++ qfirst/util/beam_filter.py
@@ -4,11 +4,14 @@ class BeamFilter():
     def __init__(self,
                  question_threshold: float,
                  span_threshold: float,
-                 invalid_threshold: float):
+                 invalid_threshold: float,
+                 invalid_as_threshold: bool = False,
+                 first_answer_only: bool = False):
         self.question_threshold = question_threshold
         self.span_threshold = span_threshold
         self.invalid_threshold = invalid_threshold
-        # TODO: first answer only, incorporation of question probability into invalidity judgment
+        self.invalid_as_threshold = invalid_as_threshold
+        self.first_answer_only = first_answer_only
 
     def __call__(self, beam):
         """
@@ -24,12 +27,20 @@ class BeamFilter():
                         return True
             return False
 
+        entries_with_spans = [(entry, s) for entry in beam for s in entry["spans"]]
         for entry in beam:
-            is_likely = entry["question_prob"] > self.question_threshold
-            is_valid = entry["invalidity_prob"] < self.invalid_threshold
-            valid_spans = [span for span, prob in entry["spans"] if prob >= self.span_threshold]
+            q_prob = entry["question_prob"]
+            invalid_prob = entry["invalidity_prob"]
+            is_likely = q_prob > self.question_threshold
+            is_valid = invalid_prob < self.invalid_threshold
+            valid_spans = [span for span, prob in entry["spans"]
+                           if prob >= self.span_threshold
+                           and ((not self.invalid_as_threshold) or prob >= invalid_prob)]
             novel_spans = [span for span in valid_spans if not has_overlap(span)]
             if(is_likely and is_valid and len(novel_spans) > 0):
+                if self.first_answer_only:
+                    only_span, _ = max(entry["spans"], key = lambda t: t[1])
+                    novel_spans = [only_span]
                 filtered_entry = {
                     "question": entry["question"],
                     "question_prob": entry["question_prob"],
@@ -44,6 +55,10 @@ class BeamFilter():
         question_threshold = params.pop("question_threshold")
         span_threshold = params.pop("span_threshold")
         invalid_threshold = params.pop("invalid_threshold")
+        invalid_as_threshold = params.pop("invalid_as_threshold", False)
+        first_answer_only = params.pop("first_answer_only", False)
         return BeamFilter(question_threshold = question_threshold,
-                         span_threshold = span_threshold,
-                         invalid_threshold = invalid_threshold)
+                          span_threshold = span_threshold,
+                          invalid_threshold = invalid_threshold,
+                          invalid_as_threshold = invalid_as_threshold,
+                          first_answer_only = first_answer_only)
