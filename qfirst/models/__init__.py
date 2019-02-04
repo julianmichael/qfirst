7e155340aed20eb7605a2b1d569d2755789141b3
julianmichael <julianjohnmichael@gmail.com>
2019-02-04 11:53:17 -0800

    Add TAN model (just generalized clause model is all)

--- qfirst/models/__init__.py
+++ qfirst/models/__init__.py
@@ -2,6 +2,5 @@ from qfirst.models.span import SpanModel
 from qfirst.models.question import QuestionModel
 from qfirst.models.question_to_span import QuestionToSpanModel
 from qfirst.models.span_to_question import SpanToQuestionModel
-# extras not used yet
-from qfirst.models.clause import ClauseModel
+from qfirst.models.multiclass import MulticlassModel
 
--- qfirst/models/multiclass.py
+++ qfirst/models/multiclass.py
@@ -20,28 +20,33 @@ from allennlp.training.metrics import SpanBasedF1Measure
 
 from qfirst.metrics.binary_f1 import BinaryF1
 
-@Model.register("qasrl_clause")
-class ClauseModel(Model):
+@Model.register("qasrl_multiclass")
+class MulticlassModel(Model):
     def __init__(self, vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  sentence_encoder: Seq2SeqEncoder,
+                 label_name: str,
+                 label_namespace: str,
                  predicate_feature_dim: int = 100,
-                 clause_hidden_dim: int = 100,
+                 prediction_hidden_dim: int = 100,
                  embedding_dropout: float = 0.0,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None):
-        super(ClauseModel, self).__init__(vocab, regularizer)
+        super(MulticlassModel, self).__init__(vocab, regularizer)
         self._text_field_embedder = text_field_embedder
         self._sentence_encoder = sentence_encoder
         self._predicate_feature_dim = predicate_feature_dim
-        self._clause_hidden_dim = clause_hidden_dim
+        self._prediction_hidden_dim = prediction_hidden_dim
         self._embedding_dropout = Dropout(p = embedding_dropout)
 
+        self._label_name = label_name
+        self._label_namespace = label_namespace
+
         self._predicate_feature_embedding = Embedding(2, predicate_feature_dim)
-        self._clause_pred = Sequential(
-            Linear(self._sentence_encoder.get_output_dim(), self._clause_hidden_dim),
+        self._final_pred = Sequential(
+            Linear(self._sentence_encoder.get_output_dim(), self._prediction_hidden_dim),
             ReLU(),
-            Linear(self._clause_hidden_dim, self.vocab.get_vocab_size("abst-clause-labels"))
+            Linear(self._prediction_hidden_dim, self.vocab.get_vocab_size(self._label_namespace))
         )
 
         embedding_dim_with_predicate_feature = self._text_field_embedder.get_output_dim() + self._predicate_feature_dim
@@ -52,15 +57,11 @@ class ClauseModel(Model):
 
         self._metric = BinaryF1()
 
-    def get_slot_names(self):
-        return self._question_generator.get_slot_names()
-
     @overrides
     def forward(self,
                 text: Dict[str, torch.LongTensor],
                 predicate_indicator: torch.LongTensor,
                 predicate_index: torch.LongTensor,
-                clause_set: torch.LongTensor,
                 **kwargs):
         # Shape: batch_size, num_tokens, embedding_dim
         embedded_text_input = self._embedding_dropout(self._text_field_embedder(text))
@@ -75,15 +76,15 @@ class ClauseModel(Model):
         # Shape: batch_size, encoder_output_dim
         pred_rep = batched_index_select(encoded_text, predicate_index).squeeze(1)
         # Shape: batch_size, get_vocab_size("abst-clause-labels")
-        clause_logits = self._clause_pred(pred_rep)
-        clause_probs = torch.sigmoid(clause_logits)
-
-        output_dict = { "clause_logits": clause_logits, "clause_probs": clause_probs }
-        if clause_set is not None:
+        logits = self._final_pred(pred_rep)
+        probs = torch.sigmoid(logits)
+        output_dict = { "logits": logits, "probs": probs }
+        if self._label_name in kwargs and kwargs[self._label_name] is not None:
+            label_set = kwargs[self._label_name]
             output_dict["loss"] = F.binary_cross_entropy_with_logits(
-                clause_logits, clause_set, reduction = "sum"
+                logits, label_set, reduction = "sum"
             )
-            self._metric(clause_probs, clause_set)
+            self._metric(probs, label_set)
         return output_dict
 
     def get_metrics(self, reset: bool = False):
