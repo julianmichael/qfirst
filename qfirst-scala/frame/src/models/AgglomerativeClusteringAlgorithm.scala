edfef9d417deefc65cb725e5a90fef4e7c127422
Julian Michael <julianjohnmichael@gmail.com>
2020-02-21 19:10:14 -0800

    Add joint/nested clustering algorithm

--- qfirst-scala/frame/src/models/AgglomerativeClusteringAlgorithm.scala
+++ qfirst-scala/frame/src/models/AgglomerativeClusteringAlgorithm.scala
@@ -137,119 +137,62 @@ trait AgglomerativeClusteringAlgorithm {
 
 }
 
-// trait JointAgglomerativeClusteringAlgorithm extends AgglomerativeClusteringAlgorithm {
-//   val innerAlgorithm: ClusteringAlgorithm
+class JointAgglomerativeClusteringAlgorithm[I, InnerIndex](
+  val innerAlgorithm: ClusteringAlgorithm { type Index = InnerIndex },
+  getSubInstances: I => NonEmptyVector[InnerIndex],
+  getLossPenalty: Int => Double // should grow monotonically
+) extends AgglomerativeClusteringAlgorithm {
+  type Index = I
+  type ClusterParam = NonEmptyVector[(MergeTree[innerAlgorithm.Index], innerAlgorithm.ClusterParam)]
 
-//   def getLossPenalty(numClusters: Int): Double
+  val innerStoppingCondition = (
+    trees: Map[Int, (MergeTree[InnerIndex], innerAlgorithm.ClusterParam)], i: Int, j: Int, newLoss: Double
+  ) => {
+    val lossBefore = trees.values.map(_._1.loss).sum + getLossPenalty(trees.size)
+    val lossAfter = (trees - i - j).values.map(_._1.loss).sum + newLoss + getLossPenalty(trees.size - 1)
+    lossAfter > lossBefore
+  }
 
-//   type Instance = Vector[innerAlgorithm.Instance]
-//   type ClusterParam = Vector[(MergeTree[(Int, Int)], innerAlgorithm.ClusterParam)]
+  def getSingleInstanceParameter(
+    index: Index,
+  ): ClusterParam = {
+    val innerIndices = getSubInstances(index)
+    innerAlgorithm.runAgglomerativeClustering(
+      innerIndices,
+      innerStoppingCondition
+    )
+  }
 
-//   def runPartialInnerClustering(
-//     trees: ClusterParam
-//   ): ClusterParam = {
-//     require(trees.size > 0)
+  def getInstanceLoss(
+    instance: Index,
+    param: ClusterParam
+  ): Double = {
+    param.foldMap(_._1.loss) + getLossPenalty(param.size.toInt)
+  }
 
-//     val distances = Array.ofDim[Double](trees.size, trees.size)
+  def mergeParams(
+    left: MergeTree[Index],
+    leftParam: ClusterParam,
+    right: MergeTree[Index],
+    rightParam: ClusterParam
+  ): ClusterParam = {
+    innerAlgorithm.runPartialAgglomerativeClustering(
+      leftParam |+| rightParam, innerStoppingCondition
+    )
+  }
 
-//     var currentTrees = trees.zipWithIndex.map(_.swap).toMap
-
-//     import scala.collection.mutable
-//     def simOrdering(i: Int) = Ordering.by[Int, (Double, Int)](j => distances(i)(j) -> j)
-//     // omit the last instance index because it's covered by all of the other queues
-//     val queues = mutable.Map(
-//       (0 until trees.size).map(i => i -> mutable.SortedMap.empty[Int, Double](simOrdering(i))): _*
-//     )
-//     currentTrees.toList.tails.toVector.foreach {
-//       case Nil => ()
-//       case (leftIndex, (left, leftParam)) :: rights =>
-//         if(rights.isEmpty) queues -= leftIndex // remove empty queue
-//         else rights.foreach { case (rightIndex, (right, rightParam)) =>
-//           val loss = mergeLoss(instances, left, leftParam, right, rightParam)
-//           val delta = getLossChangePriority(loss, left.loss, right.loss)
-//           distances(leftIndex)(rightIndex) = delta
-//           distances(rightIndex)(leftIndex) = delta
-//           queues(leftIndex) += (rightIndex -> loss)
-//         }
-//     }
-
-//     var nextRank = 1
-//     while(!stoppingCondition(currentTrees) && currentTrees.size > 1) {
-//       val i = queues.iterator.minBy { case (k, q) => distances(k)(q.firstKey) }._1
-//       val (j, newLoss) = queues(i).head
-
-//       queues -= j // remove j from consideration
-//       queues(i).clear() // clear i's queue since we'll refresh it
-//       val emptyQueues = queues.iterator.flatMap { case (k, q) =>
-//         q --= List(i, j)  // remove stale entries for i and j from all queues
-//         if(q.isEmpty && k != i) Some(k) else None // track which queues are now empty
-//       }
-//       queues --= emptyQueues // and remove other now-empty queues
-//       val (leftTree, leftParam) = currentTrees(i)
-//       val (rightTree, rightParam) = currentTrees(j)
-//       val newTree = MergeTree.Merge(nextRank, newLoss, leftTree, rightTree)
-//       val newParam = mergeParams(instances, leftTree, leftParam, rightTree, rightParam)
-//       currentTrees --= List(i, j) // remove i and j from current trees
-//       // get new merge candidates before adding new cluster back in
-//       val newMerges = currentTrees.iterator.map { case (k, (right, rightParam)) =>
-//         val loss = mergeLoss(instances, newTree, newParam, right, rightParam)
-//         val delta = getLossChangePriority(loss, newTree.loss, right.loss)
-//         // update distances before inserting anything into queues to keep things consistent
-//         distances(i)(k) = delta
-//         distances(k)(i) = delta
-//         k -> loss
-//       }.toList
-//       currentTrees += (i -> (newTree -> newParam)) // now add the new merged cluster to current trees
-//       queues(i) ++= newMerges // throw them all into queue i. don't need the symmetric case
-//       nextRank = nextRank + 1
-//     }
-//     currentTrees.values.toVector
-//   }
-
-//   def getSingleInstanceParameter(
-//     index: Int,
-//     instance: Instance
-//   ): ClusterParam = {
-//     // TODO: this has an issue: indices in inner clustering may get messed up.
-//     // probably we want to generalize the indexing of instances all over, to improve this situation.
-//     val leaves = instance.zipWithIndex.map { case (innerInstance, innerIndex) =>
-//       val param = innerAlgorithm.getSingleInstanceParameter(innerIndex, innerInstance)
-//       val loss = innerAlgorithm.getInstanceLoss(innerInstance, param)
-//       MergeTree.Leaf(loss, (index, innerIndex)) -> param
-//     }
-//     runPartialInnerClustering(leaves)
-//   }
-
-//   def getInstanceLoss(
-//     instance: Instance,
-//     param: ClusterParam
-//   ): Double = {
-//     param.map(_._1.loss).sum + getLossPenalty(param.size)
-//   }
-
-//   def mergeParams(
-//     instances: Vector[Instance],
-//     left: MergeTree[Int],
-//     leftParam: ClusterParam,
-//     right: MergeTree[Int],
-//     rightParam: ClusterParam
-//   ): ClusterParam = {
-//     runPartialInnerClustering(leftParam ++ rightParam)
-//   }
-
-//   def mergeLoss(
-//     instances: Vector[Instance],
-//     left: MergeTree[Int],
-//     leftParam: ClusterParam,
-//     right: MergeTree[Int],
-//     rightParam: ClusterParam
-//   ): Double = {
-//     val mergedParam = runPartialInnerClustering(
-//       leftParam ++ rightParam
-//     )
-//     mergedParam.map(_._1.loss).sum + getLossPenalty(param.size)
-//   }
-// }
+  def mergeLoss(
+    left: MergeTree[Index],
+    leftParam: ClusterParam,
+    right: MergeTree[Index],
+    rightParam: ClusterParam
+  ): Double = {
+    val mergedParam = innerAlgorithm.runPartialAgglomerativeClustering(
+      leftParam |+| rightParam, innerStoppingCondition
+    )
+    mergedParam.foldMap(_._1.loss) + getLossPenalty(mergedParam.size.toInt)
+  }
+}
 
 trait CompositeAgglomerativeClusteringAlgorithm extends AgglomerativeClusteringAlgorithm {
   val _1: AgglomerativeClusteringAlgorithm
